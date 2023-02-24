1ff0ab13a968037d75ae2232a503a75ac6ca02b0
JohnLi139 <malkon@mail.ru>
2023-02-24 11:16:23 +0300

    Updates iparallel.sh with CPU Util and date

--- iparallel.sh
+++ iparallel.sh
@@ -27,14 +27,15 @@ if [ ! -d "$DIR" ]; then
 fi
 # --------------------------------------------------------------------------------------------
 echo START
-echo "Version:" $(iperf3 --version | grep iperf) >> $OUT
-echo "Client: " $(hostname) >> $OUT
-echo "Server: " $1 >> $OUT
-echo "Each test duration: " $TIME >> $OUT
-echo "Send size: " $PACKET >> $OUT
+echo "Date;"$(date) >> $OUT
+echo "Version;"$(iperf3 --version | grep iperf) >> $OUT
+echo "Client;"$(hostname) >> $OUT
+echo "Server;"$1 >> $OUT
+echo "Each test duration;"$TIME >> $OUT
+echo "Send size;"$PACKET >> $OUT
 echo "" >> $OUT
 echo $(ip link show eth0 | grep mtu  | awk -F" " '{ print $4 " = " $5 }'), TCP >> $OUT
-echo "Parallel streams;Bandwidth, Gbits/sec;Retransmitted Segments, %;-;Transfer, GBytes" >> $OUT
+echo "Parallel streams;Bandwidth, Gbits/sec;Retransmitted Segments, %;-;Transfer, GBytes;CPU Util local, %;CPU Util remote, %" >> $OUT
 # ------------------------------------------------------------------------------------------------
 p=1
 TEST="TCP-P$p"
@@ -44,7 +45,7 @@ if [ -f "$FILE" ]; then
    rm -f $FILE
 fi
 iperf3 -c $1 -V -t $TIME -P $p -i 0 -f g -l $PACKET --logfile $FILE > /dev/null 2>&1
-echo $p";"$(awk -F"]" '/sender$/ { print $2 }' $FILE | awk -F" " '{ print $5 ";=" $7 "/(" $3 "*1024*1024*1024/131072);;" $3 ";" }')$(awk -F" " '/CPU/ { print $4 }' $FILE) >> $OUT
+echo $p";"$(awk -F"]" '/sender$/ { print $2 }' $FILE | awk -F" " '{ print $5 ";=" $7 "/(" $3 "*1024*1024*1024/131072);;" $3 ";" }')$(awk -F" " '/CPU/ { print $4 ";" $7 }' $FILE) >> $OUT
 sleep $PAUSE
 # ------------------------------------------------------------------------------------------------
 for ((p=2; p<=16; p++))
@@ -56,7 +57,7 @@ if [ -f "$FILE" ]; then
    rm -f $FILE
 fi
 iperf3 -c $1 -V -t $TIME -P $p -i 0 -f g -l $PACKET --logfile $FILE > /dev/null 2>&1
-echo $p";"$(awk -F"]" '/SUM.*sender$/ { print $2 }' $FILE | awk -F" " '{ print $5 ";=" $7 "/(" $3 "*1024*1024*1024/131072);;" $3 ";" }')$(awk -F" " '/CPU/ { print $4 }' $FILE) >> $OUT
+echo $p";"$(awk -F"]" '/SUM.*sender$/ { print $2 }' $FILE | awk -F" " '{ print $5 ";=" $7 "/(" $3 "*1024*1024*1024/131072);;" $3 ";" }')$(awk -F" " '/CPU/ { print $4 ";" $7 }' $FILE) >> $OUT
 sleep $PAUSE
 done
 # --------------------------------------------------------------------------------------------------
