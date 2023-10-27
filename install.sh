4c6ac3a2452e280d1e7f1eb2275015da1f846372
JohnLi139 <malkon@mail.ru>
2023-10-27 10:49:20 +0300

    deleted git clone, added iperf3 --version

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+cd $HOME
+
 # install packets and dependencies:
 yum install -y gcc gcc-c++ make automake git wget unzip
-
-# clone repository:
-cd $HOME
-git clone https://github.com/johnli139/iperf3.git
 
 # install iperf3
 wget --no-check-certificate https://codeload.github.com/esnet/iperf/zip/master -O iperf3.zip
@@ -15,4 +13,4 @@ cd iperf-master
 make
 make install
 
-cd $HOME/iperf3
+iperf3 --version | head -n1
